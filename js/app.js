4facd6040f5c3a0b16e436409894b2c799d21d8c
Maxime Hemmerlé <maxime_hemmerle@orange.fr>
2019-10-29 14:46:58 +0100

    remplacer le texte du titre1 + changer la lste active + démarrer la vidéo.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
 $(function() {
-  $("#navbarDropdown").click(function(){
+  $("#navbarDropdown").click(function(){                    //Afficher la dropdown
     $(".dropdown-menu").toggle();
   });
 
-  $("a").removeClass();
+  $(".nav-link").removeClass('disabled');                        //Supprimer la class disabled
 
   $("#search").keyup(function(){
-    $("#searchResult").html($("#search").val())
+    $("#searchResult").html($("#search").val())                   //Afficher ce qu'il y a dans "Search"
   })
+
+  $("#btn-title").click(function(){
+    var num = Math.floor((Math.random() * 1000) + 1);
+    $(".display-4").text("Web developer" + num);                                //Remplacer le texte du titre 1
+  });
+
+  $(".list-group-item").click(function(){                                   //Changer la liste active quand on clique sur une autre
+    $(this)
+      .addClass('active')
+      .siblings()
+      .removeClass("active");
+  });
+
+  $('#playVideo').click(function() {                       //démarrer la vidéo + mettre en pause au bout de 10sec.
+    $("#video")
+      .trigger('play');
+    setTimeout(function() {
+    $('#video').get(0).pause()
+  }, 11000)
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
